09d1272a7ee116e34a1aaeb12289846ee6364fae
olangriffin <olangriffin1@gmail.com>
2024-09-27 19:32:19 +0100

    Update main.py
    created an input for a name variable

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-Print("this is the first python file created in this repository"
+Print("App 1)
+print()
+name = input("Name: ")
+
+def greeting():
+    print(f"Hello {name}")
+
+def choice()
+    x = input("Do you want to continue? (y/n) ")
+    if x == y:
+        print("Ok we will continue")
+    elif x == n:
+        exit()
+    else:
+        print("incorrect input. try again")
+        choice()
+
+greeting()
+choice()
